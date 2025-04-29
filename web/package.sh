4c3a0dc18c62de813007361cc46285425e138168
BirbIrl <birbirl@proton.me>
2025-04-29 02:49:13 +0200

    added docs folder for web versions, hopefully this just works

--- web/package.sh
+++ web/package.sh
@@ -2,4 +2,5 @@
 cd ../src
 zip -r ../web/MineSweeter.love * 
 cd -
-python build.py MineSweeter.love build
+python build.py MineSweeter.love ../docs
+echo "# This isn't actually the docs, this is the game, but github pages will only (realisitically) run from this folder without a headache so for now - good enough" > ../docs/README.md
